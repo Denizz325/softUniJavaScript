8c9795e1e0ed5014ae702ceb6d7046b6219c849c
Denizz325 <09aq.abv@gmail.com>
2024-04-20 15:45:11 +0300

    updated

--- jsAdvanced/labsAndExercise/01syntaxFunctionsAndStatements/10aggregateElements.js
+++ jsAdvanced/labsAndExercise/01syntaxFunctionsAndStatements/10aggregateElements.js
@@ -1,37 +1,71 @@
 function calculateSums(arr){
-    function calculateSum(arr) {
         let sum = 0;
         for (let num of arr) {
             sum += num;
         }
-        return sum;
-    }
+        console.log(sum);
     
     
-    function calculateInverseSum(arr) {
         let inverseSum = 0;
         for (let num of arr) {
             inverseSum += 1 / num;
         }
-        return inverseSum;
-    }
+        console.log(inverseSum);
     
-    
-    function concatenateStrings(arr) {
+
         let result = '';
         for (let num of arr) {
             result += num.toString();
         }
-        return result;
-    }
+        console.log(result);
 
-    console.log(calculateSum(arr));
-    console.log(calculateInverseSum(arr));
-    console.log(concatenateStrings(arr));
 
 }
 
 calculateSums([1,2,3])
+
+
+
+
+
+
+
+
+
+// function calculateSums(arr){
+//     function calculateSum(arr) {
+//         let sum = 0;
+//         for (let num of arr) {
+//             sum += num;
+//         }
+//         return sum;
+//     }
+    
+    
+//     function calculateInverseSum(arr) {
+//         let inverseSum = 0;
+//         for (let num of arr) {
+//             inverseSum += 1 / num;
+//         }
+//         return inverseSum;
+//     }
+    
+    
+//     function concatenateStrings(arr) {
+//         let result = '';
+//         for (let num of arr) {
+//             result += num.toString();
+//         }
+//         return result;
+//     }
+
+//     console.log(calculateSum(arr));
+//     console.log(calculateInverseSum(arr));
+//     console.log(concatenateStrings(arr));
+
+// }
+
+// calculateSums([1,2,3])
 
 
 
