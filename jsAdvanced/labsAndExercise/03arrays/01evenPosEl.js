edb4a9e70cb7c77540b7f2b77fa6be2996d0b946
Denizz325 <09aq.abv@gmail.com>
2024-04-22 11:16:33 +0300

    updated

---
+++ jsAdvanced/labsAndExercise/03arrays/01evenPosEl.js
@@ -0,0 +1,10 @@
+function evenElements(arr) {
+    let result = "";
+    for (let i = 0; i < arr.length; i+=2) {
+        result += arr[i] + ' ';
+    }
+
+    console.log(result.trim())
+}
+
+evenElements([20,30,40])
