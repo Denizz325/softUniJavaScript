b63dcaad48d27a58101c3baaad63929a3ff2f056
Denizz325 <09aq.abv@gmail.com>
2024-04-24 22:57:30 +0300

    updated

--- jsAdvanced/labsAndExercise/03arrays/08processOddPos.js
+++ jsAdvanced/labsAndExercise/03arrays/08processOddPos.js
@@ -1,10 +1,13 @@
+// ne rabotiii
+
+
 function solve(arr) {
     const result = [];
 
-    for (let i = 0; i < arr.length; i+=2) {
- 
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 !== 0) {
             result.unshift(arr[i] * 2);
-        
+        }
     }
 
     formatedResult = result.join(" ")
