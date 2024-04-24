ccb662d92ee46214054f3b5476d29f187b9232fe
Denizz325 <09aq.abv@gmail.com>
2024-04-24 22:33:04 +0300

    updated

--- jsAdvanced/labsAndExercise/03arrays/05smallestTwoNums.js
+++ jsAdvanced/labsAndExercise/03arrays/05smallestTwoNums.js
@@ -1,8 +1,8 @@
 function solve(arr) {
-    const sortedArr = arr.sort((a, b) => a - b)
+    const sortedArr = arr.sort((a, b) => a - b);
     
     let removedElements = arr.splice(0, 2);
-    return removedElements
+    return removedElements;
 }
 
 solve([1,4,3,6,3,1,2,10])
