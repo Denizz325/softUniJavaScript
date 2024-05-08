f72439333655646a85605fda0ee3c395e992a2ec
Denizz325 <09aq.abv@gmail.com>
2024-05-08 18:52:30 +0300

    updated

--- jsAdvanced/labsAndExercise/08DOMExercise/01. Subtraction/subtract.js
+++ jsAdvanced/labsAndExercise/08DOMExercise/01. Subtraction/subtract.js
@@ -1,3 +1,7 @@
 function subtract() {
-    console.log('TODO:...');
+    const result = document.getElementById('result');
+    num1 = Number(document.getElementById("firstNumber").value);
+    num2 = Number(document.getElementById("secondNumber").value);
+
+    result.innerHTML = (num1 - num2).toString();
 }
--- jsAdvanced/labsAndExercise/08DOMExercise/02. Pascal or Camel Case/solution.js
+++ jsAdvanced/labsAndExercise/08DOMExercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+  text = document.getElementById("text").value;
+  nameConvention = document.getElementById('naming-convention').value;
+  const resultContainer = document.getElementById('result')
+  message = '';
+
+  const splitted = text.split(' ')
+
+  if (nameConvention == 'Camel Case') {
+    message += splitted[0][0].toLowerCase() + 
+    splitted[0].slice(1, splitted[0].length).toLowerCase();
+
+    for (let i = 1; i < splitted.length; i++) {
+      message += splitted[i][0].toUpperCase() + 
+      splitted[i].slice(1, splitted[i].length).toLowerCase();
+
+    }
+
+    resultContainer.textContent = message
+
+  } else if (nameConvention == 'Pascal Case') {
+    for (let i = 0; i < splitted.length; i++) {
+      message += splitted[i][0].toUpperCase() + 
+      splitted[i].slice(1, splitted[i].length).toLowerCase();
+
+    }
+
+    resultContainer.textContent = message
+
+  } else {
+    resultContainer.textContent = 'Error!';
+  }
+
+
+
 }
