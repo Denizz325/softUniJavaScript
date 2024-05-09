ab34da9768c77bc2bd43e7854d06b63373d0a5ea
Denizz325 <09aq.abv@gmail.com>
2024-05-09 11:31:39 +0300

    updated

--- jsAdvanced/labsAndExercise/08DOMExercise/04. Search in List/search.js
+++ jsAdvanced/labsAndExercise/08DOMExercise/04. Search in List/search.js
@@ -1,15 +1,18 @@
 function search() {
    const elements = Array.from(document.querySelectorAll('ul li'));
-
    searchedText = document.getElementById('searchText').value;
+   let matches = 0;
 
    elements.forEach((el) => {
       if (el.textContent.includes(searchedText)) {
          el.style.fontWeight = 'bold';
          el.style.textDecoration = 'underline';
+         matches ++
       } else {
          el.style.fontWeight = 'normal';
          el.style.textDecoration = '';
       }
    })
+
+   document.getElementById('result').textContent = `${matches} matches found`;
 }
