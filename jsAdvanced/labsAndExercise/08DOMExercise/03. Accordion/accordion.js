2783e243c1b8ec7a85ac8517dab236a6468326a5
Denizz325 <09aq.abv@gmail.com>
2024-05-09 10:49:23 +0300

    updated

--- jsAdvanced/labsAndExercise/08DOMExercise/03. Accordion/accordion.js
+++ jsAdvanced/labsAndExercise/08DOMExercise/03. Accordion/accordion.js
@@ -1,3 +1,12 @@
 function toggle() {
-    console.log('TODO:...');
+    text = document.getElementById('extra');
+    btn = document.querySelector('.button');
+
+    btn.textContent = btn.textContent == 'More' ? 'Less' : 'More';
+
+
+    text.style.display = 
+    text.style.display == 'none' || text.style.display == ""  ? 
+    text.style.display = 'block' : text.style.display = 'none';
+
 }
--- jsAdvanced/labsAndExercise/08DOMExercise/04. Search in List/search.js
+++ jsAdvanced/labsAndExercise/08DOMExercise/04. Search in List/search.js
@@ -1,3 +1,15 @@
 function search() {
-   // TODO
+   const elements = Array.from(document.querySelectorAll('ul li'));
+
+   searchedText = document.getElementById('searchText').value;
+
+   elements.forEach((el) => {
+      if (el.textContent.includes(searchedText)) {
+         el.style.fontWeight = 'bold';
+         el.style.textDecoration = 'underline';
+      } else {
+         el.style.fontWeight = 'normal';
+         el.style.textDecoration = '';
+      }
+   })
 }
