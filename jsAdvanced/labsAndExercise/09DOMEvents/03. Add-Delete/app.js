e2fce9b4d324e547eef5bbd53e25bcd1f2b5a43a
Denizz325 <09aq.abv@gmail.com>
2024-05-10 17:23:21 +0300

    updated

--- jsAdvanced/labsAndExercise/09DOMEvents/03. Add-Delete/app.js
+++ jsAdvanced/labsAndExercise/09DOMEvents/03. Add-Delete/app.js
@@ -1,3 +1,56 @@
 function addItem() {
-    //TODO...
+    const input = document.getElementById('newItemText');
+    const newLiElement = document.createElement('li');
+    newLiElement.textContent = input.value;
+    document.getElementById('items').appendChild(newLiElement);
+    input.value = ''
+
+
+    const newHrefEl = document.createElement('a');
+    const linkText = document.createTextNode('[Delete]')
+    newHrefEl.appendChild(linkText)
+    newHrefEl.href = '#'
+
+
+    newLiElement.appendChild(newHrefEl);
+
+    newHrefEl.addEventListener('click', deleteItem);
+
+    function deleteItem() {
+        newLiElement.remove();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+// МОЕ РЕШЕНИЕ НО JUDJE НЕ ГО ПРИЕМА 
+
+
+// function addItem() {
+//     const input = document.getElementById('newItemText');
+//     const newLiElement = document.createElement('li');
+//     newLiElement.textContent = input.value;
+//     document.getElementById('items').appendChild(newLiElement);
+//     input.value = ''
+
+
+//     const newHrefEl = document.createElement('a');
+//     newHrefEl.innerHTML = '<a href="#">[Deleted]</a>';
+
+
+//     newLiElement.appendChild(newHrefEl);
+
+//     newHrefEl.addEventListener('click', deleteItem);
+
+//     function deleteItem() {
+//         newLiElement.remove();
+//     }
+// }
