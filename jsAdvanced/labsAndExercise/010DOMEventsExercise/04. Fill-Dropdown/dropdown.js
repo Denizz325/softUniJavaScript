8d0fcf390a9003d6f97ff05896806897bd27c6ac
Denizz325 <09aq.abv@gmail.com>
2024-05-14 11:34:26 +0300

    updated

--- jsAdvanced/labsAndExercise/010DOMEventsExercise/04. Fill-Dropdown/dropdown.js
+++ jsAdvanced/labsAndExercise/010DOMEventsExercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputText = document.getElementById("newItemText");
+    const inputValue = document.getElementById("newItemValue");
+
+    dropMenu = document.getElementById("menu");
+
+    const opt = document.createElement('option')
+    opt.textContent = inputText.value
+    opt.value = inputValue.value
+    dropMenu.appendChild(opt)
+
+    inputText.value = '';
+    inputValue.value = '';
+
+    
 }
--- jsAdvanced/labsAndExercise/010DOMEventsExercise/05. Encode-and-Decode-Messages/app.js
+++ jsAdvanced/labsAndExercise/010DOMEventsExercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,51 @@
-function encodeAndDecodeMessages() {
-    console.log('TODO...')
+//Някъде има грешка
+
+
+
+function encodeAndDecodeMessages() { 
+    const Btns = document.querySelectorAll('button');
+    const encodeBtn = Btns[0].addEventListener('click', onEncode);
+    const decodeBtn = Btns[1].addEventListener('click', onDecode);
+
+    let decodeMessage = '';
+    let encodeMessage = '';
+
+    function onEncode(ev) {
+        const parent = ev.target.parentNode;
+        const message = parent.querySelector('textarea');
+        
+
+        for (let i = 0; i < message.value.length; i++) {
+            let char = message.value.charAt(i);
+            let asciiValue = char.charCodeAt(0) + 1;   
+            let shiftedValue = String.fromCharCode(asciiValue);
+
+            
+            decodeMessage += shiftedValue;      
+        }
+
+        parent.parentNode.querySelectorAll('textarea')[1].value = decodeMessage;
+        message.value = '';
+
+    }
+
+    function onDecode(ev) {
+        const parent = ev.target.parentNode;
+        const message = parent.querySelector('textarea');
+        
+
+        for (let i = 0; i < message.value.length; i++) {
+            let char = message.value.charAt(i);
+            let asciiValue = char.charCodeAt(0) - 1;   
+            let shiftedValue = String.fromCharCode(asciiValue);
+
+            
+            encodeMessage += shiftedValue;      
+        }
+
+        parent.parentNode.querySelectorAll('textarea')[1].value = encodeMessage;
+        
+
+    }
+
 }
